6202c035678df550816d455c907ba4215d11196d
Michael Diaz <mike.diaz006@gmail.com>
2019-12-20 22:01:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,8 @@
 def nyc_pigeon_organizer(data)
-  
+  new_hash = {}
+  data.each do |characteristic_category, characteristic_data| 
+    
+  end
+return new_hash
 end
 
-:purple => ["Theo", "Peter Jr.", "Lucky"],
