1dc21b1ffd5552f0963bd2b7cdb288bf40004a3d
Michael Diaz <mike.diaz006@gmail.com>
2019-12-20 22:10:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,15 +2,21 @@ require "pry"
 
 def nyc_pigeon_organizer(data)
   new_hash = {}
-  binding.pry
   data[:gender][:male].each do |name| 
     new_hash[name] = {
       :color => [],
       :gender => ["male"],
       :lives => []
-      
     }
   end
+  data[:gender][:female].each do |name| 
+    new_hash[name] = {
+      :color => [],
+      :gender => ["female"],
+      :lives => []
+    }
+  end
+  binding.pry
 return new_hash
 end
 
