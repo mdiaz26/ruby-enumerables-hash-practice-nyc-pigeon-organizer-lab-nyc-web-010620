84cdb9d514f60504e5adc6a51c8d836b2e39ca15
Michael Diaz <mike.diaz006@gmail.com>
2019-12-20 22:27:59 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,32 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data[:gender][:male].each do |name| 
+    new_hash[name] = {
+      :color => [],
+      :gender => ["male"],
+      :lives => []
+    }
+  end
+  data[:gender][:female].each do |name| 
+    new_hash[name] = {
+      :color => [],
+      :gender => ["female"],
+      :lives => []
+    }
+  end
+  data[:lives].each do |location, names|
+    names.each do |name|
+    new_hash[name][:lives] << location
+    end
+  end
+  data[:color].each do |color, names|
+    names.each do |name|
+      new_hash[name][:color] << color.to_s
+    end
+  end
+  # binding.pry
+return new_hash
 end
+
